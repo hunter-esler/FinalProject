28f539403864494a10fb935b1b6517f4c82596ce
Hunter Esler <hunter.esler@gmail.com>
2018-12-02 16:29:45 -0600

    Added getters and setters

--- app/src/main/java/edu/mtsu/hde2h/finalproject/Song.java
+++ app/src/main/java/edu/mtsu/hde2h/finalproject/Song.java
@@ -30,4 +30,84 @@ public class Song {
         trackNumber = cursor.getInt(cursor.getColumnIndex(MediaStore.Audio.Media.TRACK));
         uri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id);
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public long getAlbumId() {
+        return albumId;
+    }
+
+    public long getArtistId() {
+        return artistId;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public int getTrackNumber() {
+        return trackNumber;
+    }
+
+    public Uri getUri() {
+        return uri;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setAlbumId(long albumId) {
+        this.albumId = albumId;
+    }
+
+    public void setArtistId(long artistId) {
+        this.artistId = artistId;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public void setTrackNumber(int trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
+    public void setUri(Uri uri) {
+        this.uri = uri;
+    }
 }
